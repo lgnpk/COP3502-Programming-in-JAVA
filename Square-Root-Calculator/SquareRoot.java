2667550e7ce111830b75aea9534593a898e98607
Logan Peck <lgnpk2@gmail.com>
2017-11-02 13:34:51 -0400

    second batch

---
+++ Square-Root-Calculator/SquareRoot.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SquareRoot {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		double num = input.nextDouble();
+		Math.sqrt(input);
+		System.out.println("The Square root of the number is: ");
+		 
+	}
+}
